9782a4b627c44d1b39135cbf7a6a9fe59600beed
ValloisInes <90639314+ValloisInes@users.noreply.github.com>
2021-09-27 15:43:31 +0200

    Add files via upload

--- PilotRoues.java
+++ PilotRoues.java
@@ -1,17 +1,25 @@
 package firstApp.java;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.Motor;
-import lejos.ev3.*;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.utility.Delay;
 
-public class PilotRoues {
-DifferentialPilot pilot= new DifferentialPilot(2.1f, 4.4f,Motor.A,Motor.B,false);
-pilot.travel=5;
-pilot.travel=-20;
-pilot.travel(10);
-pilot.travel(-10);
 
-Delay.msDelay(1000);
+public class PilotRoues{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DifferentialPilot pilot= new DifferentialPilot(1.3f, 3.2f,Motor.A,Motor.B,false);
+		pilot.travel(17.8);
+		pilot.rotate(90);
+		pilot.travel(14.9);
+		pilot.rotate(90);
+		pilot.travel(17.8);
+		pilot.rotate(90);
+		pilot.travel(14.9);
+		
+		Delay.msDelay(100);
+	}
+	//AVEC PNEUS 1.6,4
 }
-
-// ceci est une modification en direct
