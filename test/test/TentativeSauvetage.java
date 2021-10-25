a88a61cb2a17a1a4662f4fa445144638f872b847
DrivePP <drivecog@gmail.com>
2021-10-25 16:22:51 +0200

    ajout dun test

--- test/test/TentativeSauvetage.java
+++ test/test/TentativeSauvetage.java
@@ -4,7 +4,9 @@ public class TentativeSauvetage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("test");
 
+System.out.println("test2");
 	}
 
 }
