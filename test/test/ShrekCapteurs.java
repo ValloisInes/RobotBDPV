b49799f25162c3e068efb939dd6f257fbff7976f
DrivePP <drivecog@gmail.com>
2021-11-08 16:10:07 +0100

    commentaires de ShrekCapteurs

--- test/test/ShrekCapteurs.java
+++ test/test/ShrekCapteurs.java
@@ -23,10 +23,10 @@ public class ShrekCapteurs {
        	//final SampleProvider distance=distances.getDistanceMode();
        	//final EV3UltrasonicSensor s1=distances;
        	final SampleProvider distance=distances.getDistanceMode();;
-       	float[] sample = new float[distance.sampleSize()];
+       	float[] sample = new float[distance.sampleSize()]; 
        	//for(int i=0;i<100;i++) {
        	distance.fetchSample(sample,0);
-       	float d=sample[0];
+       	float d=sample[0]; // affiche la distance, stoké puis overwritte dans la 1er case du tableau sample 
        	System.out.println(d);
        	
        	return d;
@@ -34,15 +34,15 @@ public class ShrekCapteurs {
  	}
  	public int couleur(){
        	//final EV3ColorSensor c=cs;
-       	cs.setFloodlight(true);
+       	cs.setFloodlight(true);  // Lumiere blanche on pour aider a la distinction des couleurs 
  
-       	final SampleProvider sp = cs.getColorIDMode();
+       	final SampleProvider sp = cs.getColorIDMode(); // necessaire
        	//final SampleProvider average = new MeanFilter(sp, 5);
-       	float [] scsample= new float[sp.sampleSize()];
-       	sp.fetchSample(scsample, 0);
-       	int cint=(int)scsample[0];
-       	System.out.println(cint); 
-       	Delay.msDelay(4000);
+       	float [] scsample= new float[sp.sampleSize()]; // necessaire
+       	sp.fetchSample(scsample, 0); //
+       	int cint=(int)scsample[0]; // ID de la couleurs 
+       	System.out.println(cint);  // renvoi ID couleurs
+       	Delay.msDelay(4000); // Delai de 4 secondes pour pouvoir lire 
        	return cint;
  	}
  	public boolean touche() {
@@ -51,17 +51,17 @@ public class ShrekCapteurs {
        	float[] sample=new float[sp.sampleSize()];
        	//for(int i=0;i<10;i++) {
        	sp.fetchSample(sample, 0);
-       	System.out.println(sample[0]);
+       	System.out.println(sample[0]); // affiche la valeur 1 = Actionné ou 0 = pas actionné 
        	Delay.msDelay(4000);
-       	return sample[0]==1.00;
+       	return sample[0]==1.00; // renvoi true si sample[0]==1 donc actionné, false sinon 
        	//}
  
  	}
  	public static void main(String[] args) {
        	ShrekCapteurs sc=new ShrekCapteurs();
-       	sc.distances();
-       	sc.couleur();
-       	sc.touche();
+     //  	sc.distances();
+      	//sc.couleur();
+      	sc.touche();
        	Delay.msDelay(7000);
  	}
 }
